970e5dda925c32bbdf756f2bfcc6ae044ba7ac6e
HenryKhoo13 <139114359+HenryKhoo13@users.noreply.github.com>
2023-12-07 07:15:07 +0000

    Adding more components

---
+++ src/app/TestPage/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import * as React from 'react';
+
+
+export default function Home() {
+  return (<>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>Test Page</div>
+    </main>
+    </>
+  )
+}
